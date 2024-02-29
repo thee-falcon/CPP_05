3246072b637b94721da757539b24b96364746225
thee-falcon <omar.makran.10@gmail.com>
2024-02-29 18:52:50 +0100

    Working on ex00

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -6,7 +6,12 @@
 /*   By: omakran <omakran@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 17:57:59 by omakran           #+#    #+#             */
-/*   Updated: 2024/02/29 17:58:00 by omakran          ###   ########.fr       */
+/*   Updated: 2024/02/29 18:38:42 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat( const std::string& name, int grade ) : _name(name), _grade(grade) {
+    
+}
--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -6,7 +6,49 @@
 /*   By: omakran <omakran@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 17:58:05 by omakran           #+#    #+#             */
-/*   Updated: 2024/02/29 18:06:47 by omakran          ###   ########.fr       */
+/*   Updated: 2024/02/29 18:52:20 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+
+class Bureaucrat
+{
+    const std::string   _name;
+    int                 _grade;
+
+public:
+    /* ####################################
+    #          Orthodox Style             #
+    # ################################### */
+
+    // constructor:
+    Bureaucrat( const std::string& name, int grade );
+    // destructor:
+    ~Bureaucrat( void );
+    // copy constructor:
+    Bureaucrat( const Bureaucrat& other );
+    // assignement operator:
+   Bureaucrat& operator=( const Bureaucrat& other );
+
+    class GradeTooHighException : public std::exception {
+        const char* what() const throw();
+    };
+
+    class GradeTooLowException : public std::exception {
+        const char* what() const throw();
+    };
+
+    std::string getName( void ) const;
+    int getGrade( void ) const;
+
+    void incrementGrade( void );
+    void decrementGrade( void );
+};
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& bureaucrat);
+
+#endif
