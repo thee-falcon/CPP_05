b75fc507f36d7b1f6b532fb5d7ee9386078bcc8c
thee-falcon <omar.makran.10@gmail.com>
2024-03-04 21:55:27 +0100

    ex01 is going on

--- ex01/Form.cpp
+++ ex01/Form.cpp
@@ -6,18 +6,31 @@
 /*   By: omakran <omakran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 16:32:18 by omakran           #+#    #+#             */
-/*   Updated: 2024/03/03 19:50:50 by omakran          ###   ########.fr       */
+/*   Updated: 2024/03/04 16:35:16 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Form.hpp"
 
-Form::Form( const std::string& name, int signGrade, int executeGrade) : _name(name), _isSigned(signGrade), _gradeRequiredToExecute(executeGrade) {
-    
+Form::Form( const std::string& name, int signGrade, int executeGrade) : _name(name), _gradeRequiredToSign(signGrade), _gradeRequiredToExecute(executeGrade) {
+    checkGrade(_gradeRequiredToSign);
+    checkGrade(_gradeRequiredToExecute);
 }
 
 Form::~Form( void ) {
     
+}
+
+Form::Form(const Form& other)
+    : _name(other._name), _isSigned(other._isSigned), _gradeRequiredToSign(other._gradeRequiredToSign), _gradeRequiredToExecute(other._gradeRequiredToExecute) {
+    // No need to checkGrade here, as the grades are assumed to be valid in the existing Form
+}
+
+Form& Form::operator=(const Form& other) {
+    if (this != &other) {
+        this->_isSigned = other.getIsSigned();
+    }
+    return (*this);
 }
 
 void Form::checkGrade(int grade) {
@@ -34,4 +47,29 @@ std::string Form::getName ( void ) const {
 
 bool    Form::getIsSigned( void ) const {
     return (_isSigned);
+}
+
+int Form::getSignGrade( void ) const {
+    return (_gradeRequiredToSign);
+}
+
+int Form::getExecuteGrade( void ) const {
+    return (_gradeRequiredToExecute);
+}
+
+void    Form::beSigned( const Bureaucrat& bureaucrat ) {
+    if (bureaucrat.getGrade() <= _gradeRequiredToSign) {
+        this->_isSigned = true;
+    } else {
+        throw GradeTooLowException();
+    }
+}
+
+void    Form::signForm( const Bureaucrat& bureaucrat ) {
+    try {
+        beSigned(bureaucrat);
+        std::cout << bureaucrat.getName() << " signed " << _name << std::endl;
+    } catch (const GradeTooLowException& e) {
+        std::cout << bureaucrat.getName() << " couldn't sign " << _name << " because " << e.what() << std::endl;
+    }
 }
