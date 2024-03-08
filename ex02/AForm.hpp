19e1ffe2e8ca67f620e3b6ef962da5e13367e5ee
thee-falcon <omar.makran.10@gmail.com>
2024-03-08 18:31:18 +0100

    HeaderFile AFormHPP is done, now I'm working on AFormCPP file

--- ex02/AForm.hpp
+++ ex02/AForm.hpp
@@ -3,10 +3,69 @@
 /*                                                        :::      ::::::::   */
 /*   AForm.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: omakran <omakran@student.42.fr>            +#+  +:+       +#+        */
+/*   By: omakran <omakran@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 17:15:08 by omakran           #+#    #+#             */
-/*   Updated: 2024/03/07 17:15:09 by omakran          ###   ########.fr       */
+/*   Updated: 2024/03/08 18:24:35 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef AFORM_HPP
+# define AFORM_HPP
+
+# include <iostream>
+# include "Bureaucrat.hpp"
+
+class Bureaucrat;
+
+// abstract class.
+class AForm
+{
+protected:
+    const std::string   _name;
+    bool                _isSign;
+    const int           _gradeRequiredToSign;
+    const int           _gradeRequiredToExecute;
+
+    // Helper function to check if the grade is within the valid range.
+    void    checkGrade(int grade);
+    
+public:
+    /* ####################################
+    #          Orthodox Style             #
+    # ################################### */
+
+    // constructor.
+    AForm(const std::string& name, int signGrade, int executeGrade);
+    // copy constructor.
+    AForm(const AForm& other);
+    // assignement operator:
+    AForm& operator=(const AForm& other);
+    // destructor.
+    virtual ~AForm();
+
+    std::string getName( void ) const;
+    bool        getIsSigned( void ) const;
+    int         getSignGrade( void ) const;
+    int         getExecuteGrade( void ) const;
+
+    void        beSigned( Bureaucrat& bureaucrat );
+
+    // Execute the form's action
+    virtual void execute( const Bureaucrat& executor ) const = 0;
+
+    // Exceptions:
+    class GradeTooHighException : public std::exception {
+        public:
+        virtual const char* what() const throw () { return "Grade too high"; }
+    };
+    
+    class GradeTooLowException : public std::exception {
+        public:
+        virtual const char* what() const throw () { return "Grade too low"; }
+    };
+};
+
+std::ostream& operator<<( std::ostream& os, const AForm& form);
+
+#endif
--- ex02/Bureaucrat.hpp
+++ ex02/Bureaucrat.hpp
@@ -3,10 +3,65 @@
 /*                                                        :::      ::::::::   */
 /*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: omakran <omakran@student.42.fr>            +#+  +:+       +#+        */
+/*   By: omakran <omakran@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 17:15:16 by omakran           #+#    #+#             */
-/*   Updated: 2024/03/07 17:15:17 by omakran          ###   ########.fr       */
+/*   Updated: 2024/03/08 18:25:00 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>  // Include for std::ostream
+# include <string>    // Include for std::string
+# include "Form.hpp"
+
+// forward declaration:
+class Form;
+
+class Bureaucrat
+{
+    const std::string   _name;
+    int                 _grade;
+
+    // Helper function to check if the grade is within valid range.
+    void    checkGrade( void );
+
+public:
+    /* ####################################
+    #          Orthodox Style             #
+    # ################################### */
+
+    // constructor:
+    Bureaucrat( const std::string& name, int grade );
+    // destructor:
+    ~Bureaucrat( void );
+    // copy constructor:
+    Bureaucrat( const Bureaucrat& other );
+    // assignement operator:
+   Bureaucrat& operator=( const Bureaucrat& other );
+
+    // Exceptions:
+    class GradeTooHighException : public std::exception {
+        public:
+            virtual const char* what() const throw() { return "Grade too high"; }
+    };
+    class GradeTooLowException : public std::exception {
+        public:
+            virtual const char* what() const throw() { return "Grade too low"; }
+    };
+    
+    std::string getName( void ) const;
+    int         getGrade( void ) const;
+
+    void        signForm( Form& Form );
+
+    // Increment and decrement Functions: 
+    void        incrementGrade( void );
+    void        decrementGrade( void );
+};
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& bureaucrat);
+
+#endif
