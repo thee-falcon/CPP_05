bc630807de4b68436e375d687f254273718356ae
thee-falcon <omar.makran.10@gmail.com>
2024-03-09 22:38:32 +0100

    PresidentialPardonForm is done

--- ex02/PresidentialPardonForm.cpp
+++ ex02/PresidentialPardonForm.cpp
@@ -6,7 +6,37 @@
 /*   By: omakran <omakran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 17:15:30 by omakran           #+#    #+#             */
-/*   Updated: 2024/03/07 17:15:31 by omakran          ###   ########.fr       */
+/*   Updated: 2024/03/09 22:36:41 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PresidentialPardonForm.hpp"
+
+PresidentialPardonForm::PresidentialPardonForm(const std::string& target)
+    : AForm("Presidential Pardon Form", 25, 5), _target(target) {
+}
+
+PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm& other)
+    : AForm(other), _target(other._target) {
+}
+
+PresidentialPardonForm& PresidentialPardonForm::operator=(const PresidentialPardonForm& other) {
+    if (this != &other) {
+        AForm::operator=(other);
+        _target = other._target;
+    }
+    return (*this);
+}
+
+PresidentialPardonForm::~PresidentialPardonForm() {
+}
+
+void PresidentialPardonForm::execute(const Bureaucrat& executor) const {
+    AForm::execute(executor);
+
+    std::cout << _target << " has been pardoned by Zaphod Beeblebrox." << std::endl;
+}
+
+std::string PresidentialPardonForm::getTarget() const {
+    return (_target);
+}
--- ex02/PresidentialPardonForm.hpp
+++ ex02/PresidentialPardonForm.hpp
@@ -6,7 +6,36 @@
 /*   By: omakran <omakran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 17:15:36 by omakran           #+#    #+#             */
-/*   Updated: 2024/03/07 17:15:37 by omakran          ###   ########.fr       */
+/*   Updated: 2024/03/09 22:37:27 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PRESIDENTIAL_PARDON_FORM_HPP
+#define PRESIDENTIAL_PARDON_FORM_HPP
+
+#include "AForm.hpp"
+
+class PresidentialPardonForm : public AForm {
+public:
+    /* ####################################
+    #          Orthodox Style             #
+    # ################################### */
+
+    // constructor:
+    PresidentialPardonForm(const std::string& target);
+    // copy constructor:
+    PresidentialPardonForm(const PresidentialPardonForm& other);
+    // assignment operator:
+    PresidentialPardonForm& operator=(const PresidentialPardonForm& other);
+    // destructor:
+    virtual ~PresidentialPardonForm();
+
+    void execute(const Bureaucrat& executor) const override;
+
+    std::string getTarget() const;
+
+private:
+    std::string _target;
+};
+
+#endif
